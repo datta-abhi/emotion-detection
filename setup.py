51d748b959734d2c7c8251a4c86307070521d9ea
datta-abhi <rookbee91@gmail.com>
2024-06-25 20:46:11 +0530

    initialized project using cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML project to predict emotion from twitter posts using MLOPs principles',
+    author='abhigyan',
+    license='',
+)
